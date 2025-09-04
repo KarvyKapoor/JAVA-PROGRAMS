920eb0bf59b0bd193c4b9733d51236a5ef2a1bad
KarvyKapoor <karvy.kapoor2006@gmail.com>
2025-09-04 19:34:35 +0530

    Add files via upload

---
+++ SumOfNnumber.java
@@ -0,0 +1,15 @@
+package BASICS;
+import java.util.Scanner;
+
+public class SumOfNnumber {
+        public static void main(String args[]) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter an integer :");
+        int number = scanner.nextInt();
+        int sum = 0;
+        for(int i=1;i<=number;i++) {
+            sum = sum + i;
+        }
+        System.out.println("Sum Of All Numbers : "+ sum);
+    }
+}
