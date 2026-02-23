321cd367fbd6381ec75fe82e3d71b063954d9f80
Karvy Kapoor <karvy.kapoor2006@gmail.com>
2026-02-23 19:08:23 +0530

    Updated project

--- DSA/ReverseArray.java
+++ DSA/ReverseArray.java
@@ -13,7 +13,12 @@ public class ReverseArray {
             a[i]=sc.nextInt();
         }
         System.out.println("Reverse of array is :");
-        for(int i=n-1;i>=0;i--){
+        for(int i=0;i<a.length/2;i++){
+            int temp = a[i];
+            a[i] = a[a.length-1-i];
+            a[a.length-1-i] = temp;
+        }
+        for(int i=0;i<a.length;i++){
             System.out.print(a[i]+" ");
         }
         sc.close();
